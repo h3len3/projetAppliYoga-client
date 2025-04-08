570c01a61547294949dbe084389f032bced6900f
helene6789 <lenehe099@gmail.com>
2025-04-08 17:47:08 +0200

    fullcalendar

--- src/app/admin/pages/admin-calendar/admin-calendar.component.ts
+++ src/app/admin/pages/admin-calendar/admin-calendar.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
 
+//calendar
+
+import { FullCalendarModule } from '@fullcalendar/angular';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import { CalendarOptions } from '@fullcalendar/core';
+
+import { CommonModule } from '@angular/common';
+
+//FullCalendarModule.registerPlugins([dayGridPlugin]);
+// fin calendar
+
 @Component({
-  imports: [],
+  imports: [CommonModule, FullCalendarModule],
   templateUrl: './admin-calendar.component.html',
-  styleUrl: './admin-calendar.component.scss'
+  styleUrl: './admin-calendar.component.scss',
+
+  selector: 'app-calendar',
+  standalone: true,
+
+  //imports: [CommonModule, FullCalendarModule],
+  // template: `
+    //<full-calendar [options]="calendarOptions"></full-calendar>
+  //`,
+
+  template: `
+  <full-calendar [options]="calendarOptions"></full-calendar>
+`,
 })
 export class AdminCalendarComponent {
+  calendarOptions: CalendarOptions = {
+    initialView: 'dayGridMonth',
+    events: [
+      { title: 'Événement 1', date: '2025-04-08' },
+      { title: 'Événement 2', date: '2025-04-12' }
+    ]
+  };
+  
 
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,30 @@ import { RouterOutlet } from '@angular/router';
 import { Toast } from 'primeng/toast';
 import { ConfirmDialog } from 'primeng/confirmdialog';
 
+// calendar
+import { FullCalendarModule } from '@fullcalendar/angular';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import { CalendarOptions } from '@fullcalendar/core';
+
+import { CommonModule } from '@angular/common';
+
+//FullCalendarModule.registerPlugins([dayGridPlugin]);
+// fin calendar
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Toast, ConfirmDialog],
+  imports: [RouterOutlet, Toast, ConfirmDialog, CommonModule, FullCalendarModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+
+  //
+  standalone: true,
+  
+  // template: `
+    // <full-calendar [options]="calendarOptions"></full-calendar>
+
+    
+  
 })
 export class AppComponent {
   title = 'projetYoga.client';
